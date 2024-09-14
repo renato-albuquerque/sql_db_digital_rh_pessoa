ffe6dcce0fd71107569cd2bd463e5a3f37d3fb33
renato albuquerque <renatomalb@hotmail.com>
2024-09-14 20:32:28 -0300

    updated sql file

--- sql_db_digital_rh_pessoa.sql
+++ sql_db_digital_rh_pessoa.sql
@@ -8,32 +8,32 @@
 
 -- 3ª etapa, no SCHEMA rh, criar a tabela: pessoa.
 
--- CREATE TABLE IF NOT EXISTS rh.pessoa (
--- 	id serial NOT NULL PRIMARY KEY,
--- 	nome character varying,
--- 	cpf character varying,
--- 	email character varying,
--- 	nascimento date,
--- 	renda numeric (18,2),
--- 	casa_propria boolean
--- );
+CREATE TABLE IF NOT EXISTS rh.pessoa (
+ 	id serial NOT NULL PRIMARY KEY,
+ 	nome character varying,
+ 	cpf character varying,
+ 	email character varying,
+ 	nascimento date,
+ 	renda numeric (18,2),
+ 	casa_propria boolean
+);
 
 -- Checar as colunas e tipos de dados que foram criados.
--- SELECT * FROM rh.pessoa;
+SELECT * FROM rh.pessoa;
 
 -- 4ª etapa, inserir valores na tabela: rh.pessoa.
 
--- INSERT INTO rh.pessoa (nome, cpf, email, nascimento, renda, casa_propria) VALUES
--- ('Pedro', '01234567890', 'pedro@digital.com.br', '1990-01-01', 2450.75, true),
--- ('Renato', '01234567899', 'renato@digital.com.br', '1983-04-13', 5000.00, true);
+INSERT INTO rh.pessoa (nome, cpf, email, nascimento, renda, casa_propria) VALUES
+('Pedro', '01234567890', 'pedro@digital.com.br', '1990-01-01', 2450.75, true),
+('Renato', '01234567899', 'renato@digital.com.br', '1983-04-13', 5000.00, true);
 
 -- Checar valores inseridos na tabela: rh.pessoa.
--- SELECT * FROM rh.pessoa;
+SELECT * FROM rh.pessoa;
 
 -- 5ª etapa, visualizar apenas uma coluna.
--- SELECT nome FROM rh.pessoa;
+SELECT nome FROM rh.pessoa;
 
 -- 6ª etapa, visualizar duas colunas.
--- SELECT nome, cpf FROM rh.pessoa;
+SELECT nome, cpf FROM rh.pessoa;
 
 
